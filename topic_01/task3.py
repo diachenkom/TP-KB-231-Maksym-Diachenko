da579bfa928a3ae1f41f83fe0db1779c94a89915
kb-231-diachenko <diachenkom51@gmail.com>
2024-10-04 16:03:37 +0300

    add

--- topic_01/task3.py
+++ topic_01/task3.py
@@ -1,6 +1,9 @@
+def discr(a, b, c):
+    return b**2 - 4*a*c
+
 print("Quadratic equation has the form: a*(x**2) + b*x + c = 0")
 a = int(input("Enter a = "))
 b = int(input("Enter b = "))
 c = int(input("Enter c = "))
-discr = b**2 - 4*a*c
-print("Discriminant is:" + str(discr))
+d = discr(a, b, c)
+print("Discriminant is: " + str(d))
--- topic_02/task1.py
+++ topic_02/task1.py
@@ -4,15 +4,25 @@ print("Quadratic equation has the form: a*(x^2) + b*x + c = 0")
 a = int(input("Enter a = "))
 b = int(input("Enter b = "))
 c = int(input("Enter c = "))
-discr = b**2 - 4*a*c
 
-if discr > 0:
-    x1 = ((-b + math.sqrt(discr)) / 2*a)
-    x2 = ((-b - math.sqrt(discr)) / 2*a)
-    print("Equation has two solutions:")
-    print("x1 = " + str(x1) + " and x2 = " + str(x2))
-elif discr == 0:
-    x1 = -(b / 2*a)
-    print("Equation has one solutions, x is:" + x1)
-else:
-    print("Equation has not solutions")
+def discr(a, b, c):
+    return b**2 - 4*a*c
+
+def equation_solution(a, b, c):
+    d = discr(a, b, c)
+    print("Discriminant is: " + str(d))
+    if d > 0:
+        x1 = (-(b) + math.sqrt(d)) / (2*a)
+        x2 = (-(b) - math.sqrt(d)) / (2*a)
+        return f"Equation has two solutions: x1 = {x1} and x2 = {x2}"
+    elif d == 0:
+        x1 = (-b) / (2*a)
+        return f"Equation has one solutions: x = {x1}"
+    else:
+        return "Equation has not solutions"
+
+print (equation_solution(a, b, c))
+
+    #  Два розвязки: a = 7, b = -6, c = -1, d = 64, x1 = 1, x2 = -0.142857
+    #  Один розвязок: a = 25, b = -10, c = 1, d = 0, x = 0.2
+    #  Немає розвязків: a = 1, b = 1, c = 9, d = -35
