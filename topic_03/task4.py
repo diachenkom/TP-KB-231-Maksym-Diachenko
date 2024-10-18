1460ed33bb7ec3841689f1286ffc3fdc22a28f04
kb-231-diachenko <diachenkom51@gmail.com>
2024-10-18 13:39:44 +0300

    Change report 18-10-24

--- topic_03/task4.py
+++ topic_03/task4.py
@@ -4,27 +4,25 @@ print(list1)
 
 num = int(input("Введіть ціле число: ")) 
 
-for i in range(0, len(list1)-1):
-# Якщо число менше 0-го елементу списку
-    if num < list1[i]:
-        list1.insert(0, num)
-        print("Вставлено елемент з індексом: 0")
-        break
-# Якщо число рівне одному з елементів
-    if num == list1[i]:
-        list1.insert(i, num)
-        print("Вставлено елемент з індексом: ",i)
-        break
-# Якщо число в діапазоні між 2-ма сусідніми елем-ми    
-    if (list1[i] < num) and (num < list1[i+1]):
-        list1.insert(i+1, num)
-        print("Вставлено елемент з індексом: ",i+1)
-        break                  
-else:
-    list1.append(num)
-    print("Додано до списку елемент з індексом: ",len(list1)-1)
+def find_position():
+    for i in range(0, len(list1)-1):
+        if num < list1[i]:
+            list1.insert(0, num)
+            print("Вставлено елемент з індексом: 0")
+            break
+        if num == list1[i]:
+            list1.insert(i, num)
+            print("Вставлено елемент з індексом: ",i)
+            break
+        if (list1[i] < num) and (num < list1[i+1]):
+            list1.insert(i+1, num)
+            print("Вставлено елемент з індексом: ",i+1)
+            break                  
+    else:
+        list1.append(num)
+        print("Додано до списку елемент з індексом: ",len(list1)-1)
 
-print("Cписок після вставки елемента:")
-print(list1)
+    print("Cписок після вставки елемента:")
+    print(list1)
 
-
+find_position()
