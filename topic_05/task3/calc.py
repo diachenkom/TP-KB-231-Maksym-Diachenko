ad77b72b98924d4efb85081d8af20e9d28725eac
kb-231-diachenko <diachenkom51@gmail.com>
2024-11-15 10:47:12 +0200

    Add topic_5 with gitignore

---
+++ topic_05/task3/calc.py
@@ -0,0 +1,20 @@
+from operations import get_numbers, choose_operation, perform_operation
+
+def main():
+    print("Ласкаво просимо до калькулятора!")
+    while True:
+        a, b = get_numbers()
+        choice = choose_operation()
+        if choice.lower() == 'exit':
+            break
+        try:
+            result = perform_operation(choice, a, b)
+            if result is not None:
+                print("Результат:", result)
+        except ValueError as e:
+            print(e)
+        
+        continue
+        
+if __name__ == "__main__":
+    main()
